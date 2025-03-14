bcaaf3954b3aa225a260cbf240c90127922906a8
kavyasri-gaddipati <106828619+kavyasri-gaddipati@users.noreply.github.com>
2025-03-14 11:23:27 +0530

    Time: 0 ms (100%), Space: 12.5 MB (13.81%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        # Define mappings of integer values to Roman numeral symbols
+        value_symbols = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"),
+            (1, "I")
+        ]
+
+        roman_numeral = ""
+        
+        # Convert number to Roman numeral
+        for value, symbol in value_symbols:
+            while num >= value:
+                roman_numeral += symbol
+                num -= value
+        
+        return roman_numeral
