4e5c8c159a4c556d685319622d6a90b103b223c0
kavyasri-gaddipati <106828619+kavyasri-gaddipati@users.noreply.github.com>
2025-04-26 15:54:28 +0530

    Time: 0 ms (100%), Space: 12.5 MB (27.52%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,22 +4,14 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        # Edge case: If the list is empty, return an empty string
-        if not strs:
-            return ""
+        strs.sort()
+        first=strs[0]
+        last=strs[-1]
+        result=[]
+        for i in range(min(len(first),len(last))):
+            if first[i]!=last[i]:
+                break 
+            result.append(first[i]) 
+        return ''.join(result)    
+
         
-        # Assume the first string is the common prefix
-        prefix = strs[0]
-        
-        # Compare the assumed prefix with each string in the array
-        for s in strs[1:]:
-            # Check if the current prefix matches the start of the string `s`
-            while not s.startswith(prefix):
-                # If not, shorten the prefix by removing the last character
-                prefix = prefix[:-1]
-                
-                # If the prefix becomes empty, return an empty string
-                if not prefix:
-                    return ""
-        
-        return prefix
